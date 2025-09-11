3fbedda52478de6514a0c869de741f86fe9cfa06
Yoseph won <yosephwondimu26@gmail.com>
2025-09-11 15:49:44 +0300

    working on checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,7 +10,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 author = Author.objects.get(name="J.K. Rowling")
 print("Books by", author.name, ":", [book.title for book in author.books.all()])
 
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name=library_name)
 print("Books in", library.name, ":", [book.title for book in library.books.all()])
 
 library = Library.objects.get(name="Central Library")
