e8a7e47e25368af2c3fe86204f7b3e50ca0cfaff
Yoseph won <yosephwondimu26@gmail.com>
2025-09-13 17:29:20 +0300

    checking the necessity of django env't settings

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,3 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProject.settings")  
-django.setup()
-
 from relationship_app.models import Author, Book, Library, Librarian
 
 author = Author.objects.get(name=author_name)
@@ -13,3 +7,6 @@ library = Library.objects.get(name=library_name)
 print("Books in", library.name, ":", [book.title for book in library.books.all()])
 
 Librarian.objects.get(library=)
+
+
+author = Author.objects.get(name=author_name)
