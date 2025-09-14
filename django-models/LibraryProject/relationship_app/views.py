aac3548d690a1cc569598905696f8926e8d11b23
Yoseph won <yosephwondimu26@gmail.com>
2025-09-14 22:23:54 +0300

    First url configuring

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Author
 
-# Create your views here.
+def all_books(request):
+    return HttpResponse("hello world")
