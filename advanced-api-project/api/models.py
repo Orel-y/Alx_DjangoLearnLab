5cb6ff9489c1e4a25c6a67b7347d22dd53cc66ef
Yoseph won <yosephwondimu26@gmail.com>
2025-10-03 14:32:55 +0300

    Finishing Task 1. Creating models and serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
@@ -9,6 +10,8 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
+    publication_year = models.PositiveSmallIntegerField(
+        default=datetime.now().year
+    )
     def __str__(self):
         return f'{self.title} by {self.author.name}'
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Author, Book
+from datetime import datetime
+
+"""
+    AuthorSerializer and BookSerializer class Defined here
+    to convert our python MODELS to JSON format so we can 
+    work with the Client and Server side easily.
+    
+"""
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Author
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+
+    class Meta():
+        model = Book
+        fields = '__all__'
+
+    """
+        VALIDATING: validating the publication_year data
+        so Clients will not send invalid data.
+    """
+    
+    def validate(self, data):
+        publication_year = data.get('publication_year')
+        if publication_year and publication_year > datetime.now().year:
+            raise serializers.ValidationError({
+                "publication_year": "What Now! are you a time Traveler fill the form corrctly :("
+            })
+
+        return data
