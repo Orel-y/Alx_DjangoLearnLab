a37abacbf84ac8ba96dcf0f2cf2125e2e211da42
Yoseph won <yosephwondimu26@gmail.com>
2025-10-03 15:18:47 +0300

    Task 2: Creating Views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author__name']
+
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.AllowAny]
+
+class BookUpdateView(generics.UpdateAPIview):
+    query_set = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookCreateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
+    #saves automatically who created the books
+    def perform_create(self, serializer):
+        serializer.save()
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    permission_classes = [permissions.IsAuthenticated]
+
