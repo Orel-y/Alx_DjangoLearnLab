d5ec94c43f41bdb432a5582e06257389d98141cb
Yoseph won <yosephwondimu26@gmail.com>
2025-10-10 00:16:03 +0300

    Change endpoint conventions as required by the checker

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('/register', views.RegisterView.as_view(), name='register'),
-    path('/login', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
     path('/profile', views.ProfileView.as_view(), name='profile'),
 ]
