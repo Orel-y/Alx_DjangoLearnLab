47e171200fdab8e9c64800a34ce617eaa85a7ff8
Yoseph won <yosephwondimu26@gmail.com>
2025-10-09 23:48:30 +0300

    Create custom user, write serializers, and implement authentication with necessary measures

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from accounts.models import CustomUser
+from django.contrib.auth import authenticate
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        field = [
+            'id', 
+            'username', 
+            'email', 
+            'password', 
+            'bio', 
+            'profile_picture'
+        ]
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+            
+        raise serializers.ValidationError("Invalid credentials")
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+class RegisterView:
+    pass
+
+class LoginView:
+    pass
