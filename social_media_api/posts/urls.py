cda7ab9ee15d8c642ac2fa42617e2314a631ad78
Yoseph won <yosephwondimu26@gmail.com>
2025-10-11 15:59:31 +0300

    Created views for Post and Comment models

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = router.urls
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from posts.models import Post, Comment
+from posts.serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    qs = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    qs = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
