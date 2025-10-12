7ca9ce8953f599f0b247cbe9ae4923b452929a32
Yoseph won <yosephwondimu26@gmail.com>
2025-10-12 11:44:34 +0300

    Add trailing slash to endpoints in urls.py as required by the checker

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
-    path('users/follow/<int:user_id>', FollowUserAPIView.as_view(), name='follow-user'),
-    path('users/unfollow/<int:user_id>', UnfollowUserAPIView.as_view(), name='unfollow-user'),
+    path('users/follow/<int:user_id>/', FollowUserAPIView.as_view(), name='follow-user'),
+    path('users/unfollow/<int:user_id>/', UnfollowUserAPIView.as_view(), name='unfollow-user'),
 ]
